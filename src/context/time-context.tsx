e8e6bbde31b55e5a89778f7c41e7003c3c43890c
Danil <yalexxer228666@mail.ru>
2025-10-01 00:16:21 +0400

    fix | React Hook useEffect has a missing dependency

--- src/context/time-context.tsx
+++ src/context/time-context.tsx
@@ -4,7 +4,7 @@ import { createContext } from "react";
 
 type TimeContextType = {
   seconds: number;
-  formatTime: (sec: number) => string
+  formatTime: (sec: number) => string;
 };
 
 const TimeContext = createContext<TimeContextType | undefined>(undefined);
@@ -14,29 +14,34 @@ export function TimeContextProvider({ children }: { children: ReactNode }) {
   const ref = useRef<number | null>(null);
 
   useEffect(() => {
-    if (seconds <= 0) return;
-
     ref.current = window.setInterval(() => {
-      setSeconds((s) => (s > 0 ? s - 1 : 0));
+      setSeconds((s) => {
+        if (s <= 1) {
+          if (ref.current) clearInterval(ref.current);
+          return 0;
+        }
+        return s - 1;
+      });
     }, 1000);
 
     return () => {
       if (ref.current) clearInterval(ref.current);
     };
   }, []);
+
   const formatTime = (sec: number) => {
-    const m = Math.floor(sec / 60)
-      .toString()
-      .padStart(2, "0");
+    const m = Math.floor(sec / 60).toString().padStart(2, "0");
     const s = (sec % 60).toString().padStart(2, "0");
     return `${m}:${s}`;
   };
+
   return (
-    <TimeContext.Provider value={{ seconds , formatTime}}>
+    <TimeContext.Provider value={{ seconds, formatTime }}>
       {children}
     </TimeContext.Provider>
   );
 }
+
 export function useTimeContext() {
   const context = useContext(TimeContext);
   if (!context) {
